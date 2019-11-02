b155949fcbee442cb7e0f6cfe18a24e264b66bf2
nwagalla1 <nixonwagallah@gmail.com>
2019-11-02 20:45:34 +0300

    added smooth scrolling to landing page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    
+    $('.mouse').click(function(e){
+        
+        var linkHref = $(this).attr('href');
+        
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top
+        }, 800);
+
+        e.preventDefault();
+
+    });
 });
