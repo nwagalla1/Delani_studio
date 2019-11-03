ab44c70c3a9f42c23da4a39876d22b158f0e1805
nwagalla1 <nixonwagallah@gmail.com>
2019-11-03 11:46:26 +0300

    incorporated the popup message on button click

--- js/main.js
+++ js/main.js
@@ -17,3 +17,12 @@ $(document).ready(function(){
         hover: true,
     });
 });
+
+document.getElementById('button').addEventListener('click', function(){
+       
+    document.querySelector('.popup').style.display = "flex";
+})
+
+document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector('.popup').style.display = "none";
+})
