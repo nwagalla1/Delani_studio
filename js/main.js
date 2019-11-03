33589320cefe73b48f038d79f03b88aae9032e3f
nwagalla1 <nixonwagallah@gmail.com>
2019-11-03 15:25:42 +0300

    added hide and show to the what we do section

--- js/main.js
+++ js/main.js
@@ -26,3 +26,33 @@ document.getElementById('button').addEventListener('click', function(){
 document.querySelector('.close').addEventListener('click', function(){
     document.querySelector('.popup').style.display = "none";
 })
+document.getElementById('design').addEventListener('click', function(e){
+    document.querySelector('#design').style.display = 'none';
+    document.querySelector('.para').style.display = 'block';
+
+    e.preventDefault();
+})
+document.getElementById('words1').addEventListener('click', function(){
+    document.querySelector('.para').style.display = 'none';
+    document.querySelector('#design').style.display = 'block';
+})
+document.getElementById('develop').addEventListener('click', function(e){
+    document.querySelector('#develop').style.display = 'none';
+    document.querySelector('.para1').style.display = 'block';
+
+    e.preventDefault();
+})
+document.getElementById('words2').addEventListener('click', function(){
+    document.querySelector('.para1').style.display = 'none';
+    document.querySelector('#develop').style.display = 'block';
+})
+document.getElementById('manage').addEventListener('click', function(e){
+    document.querySelector('#manage').style.display = 'none';
+    document.querySelector('.para2').style.display = 'block';
+
+    e.preventDefault();
+})
+document.getElementById('words3').addEventListener('click', function(){
+    document.querySelector('.para2').style.display = 'none';
+    document.querySelector('#manage').style.display = 'block';
+})
